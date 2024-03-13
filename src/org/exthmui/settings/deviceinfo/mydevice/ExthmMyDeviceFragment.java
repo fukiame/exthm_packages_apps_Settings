c9a5fc721191d50c2b1b77328dd97cc29a78e6d2
Yuyuko1024 <dingwenxuan4@gmail.com>
2024-03-14 00:01:52 +0800

    Settings: Implement RegulatoryInfo to ExthmMyDeviceFragment
    * It is also used to read Boolean values within the resource to prevent users from entering the Activity by mistake

--- src/org/exthmui/settings/deviceinfo/mydevice/ExthmMyDeviceFragment.java
+++ src/org/exthmui/settings/deviceinfo/mydevice/ExthmMyDeviceFragment.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2019-2023 The exTHmUI Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.exthmui.settings.deviceinfo.mydevice;
 
 import android.app.settings.SettingsEnums;
@@ -14,6 +30,7 @@ import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.search.SearchIndexable;
 
 import org.exthmui.settings.deviceinfo.firmwareversion.ExthmDisplayVersionPreferenceController;
+import org.exthmui.settings.deviceinfo.mydevice.RegulatoryInfoPreferenceController;
 import org.exthmui.settings.deviceinfo.hardwareinfo.ExthmHardwareInfoPreferenceController;
 
 import java.util.ArrayList;
@@ -63,6 +80,7 @@ public class ExthmMyDeviceFragment extends DashboardFragment
     private static List<AbstractPreferenceController> buildPreferenceControllers(
             Context context, ExthmMyDeviceFragment fragment, Lifecycle lifecycle) {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new RegulatoryInfoPreferenceController(context));
         return controllers;
     }
 
