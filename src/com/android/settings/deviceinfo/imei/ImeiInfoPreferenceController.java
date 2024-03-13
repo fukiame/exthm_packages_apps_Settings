exTHmUI/android_packages_apps_Settings

Settings: Introduce My Device page
Signed-off-by: Yuyuko1024 <dingwenxuan4@gmail.com>
Change-Id: I913b6f1ff517bbe2b35b39fa03804572998aded6

src/com/android/settings/deviceinfo/imei/ImeiInfoPreferenceController.java
@@ -76,7 +76,7 @@ public class ImeiInfoPreferenceController extends BasePreferenceController {
             final Preference multiSimPreference = createNewPreference(screen.getContext());
             multiSimPreference.setOrder(imeiPreferenceOrder + simSlotNumber);
             multiSimPreference.setKey(getPreferenceKey() + simSlotNumber);
-            category.addPreference(multiSimPreference);
+            screen.addPreference(multiSimPreference);
             mPreferenceList.add(multiSimPreference);
             updatePreference(multiSimPreference, simSlotNumber);
         }

fukiame/exthm_packages_apps_Settings

src/com/android/settings/deviceinfo/imei/ImeiInfoPreferenceController.java
@@ -103,7 +103,7 @@ public class ImeiInfoPreferenceController extends BasePreferenceController {
             multiImeiPreference.setKey(DEFAULT_KEY + (1 + simSlotNumber));
             multiImeiPreference.setEnabled(true);
             multiImeiPreference.setCopyingEnabled(true);
-            category.addPreference(multiImeiPreference);
+            screen.addPreference(multiImeiPreference);
        }
     }
 